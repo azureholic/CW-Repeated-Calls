8d5a6c2a2fdb89b0489ac8a7405dbf0f36bfaf40
JorisBurghgraef <joris.burghgraef@kpn.com>
2025-05-13 14:05:04 +0200

    Added the scenario's information (dropdown menu)

--- kloten.py
+++ kloten.py
@@ -1,10 +1,16 @@
-names = ['alice', 'bob', 'charlie', 'david', 'eve', 'frank', 'grace', 'hannah', 'ian', 'jane']
-
-print([name for name in names])
 
 
+import json
+
+
+with open('/home/burgh512/Python_files/Agentic-AI/CW-Repeated-Calls/data/scenarios/scenario_specifications_test.json', 'r') as file:
+    pay_load = json.load(file)
 
 
 
+
+print(len(pay_load[0]['scenario_details']['recommended_system_response']))
+print(type(pay_load[0]['scenario_details']['recommended_system_response']))
+print((pay_load[0]['scenario_details']['recommended_system_response'])[0])
 
 
