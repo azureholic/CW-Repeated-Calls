a1713c67495de896a140b72ffae7a7716833dd37
Daan Rosendal <daanrosendal@outlook.com>
2025-05-20 01:08:40 +0200

    add local evaluation script and findings

--- evaluations/scripts/1_local_evaluation.py
+++ evaluations/scripts/1_local_evaluation.py
@@ -3,7 +3,8 @@ import datetime
 from pathlib import Path
 
 from azure.ai.evaluation import (
-    SimilarityEvaluator,
+    GroundednessEvaluator,
+    RelevanceEvaluator,
     evaluate,
 )
 from dotenv import load_dotenv
@@ -24,7 +25,7 @@ base_config = {
 }
 
 # Define model deployments that will be used to judge the quality of the responses
-model_judge_deployments = ["gpt-4o", "gpt-4o-mini", "Phi-4"]
+model_judge_deployments = ["gpt-4o"]  # , "gpt-4o-mini", "Phi-4"]
 
 # Create model configurations by combining base config with specific deployments
 model_judge_configs = {
@@ -43,7 +44,8 @@ def get_output_path(dataset_path: str, model_name: str) -> str:
 def run_evaluation(dataset_path: str):
     """Run evaluation for all models on the given dataset."""
     for model_judge_name, model_judge_config in model_judge_configs.items():
-        similarity_eval = SimilarityEvaluator(model_judge_config)
+        groundedness_eval = GroundednessEvaluator(model_judge_config)
+        relevance_eval = RelevanceEvaluator(model_judge_config)
 
         output_path = get_output_path(dataset_path, model_judge_name)
         evaluation_name = output_path.replace("results/", "")
@@ -51,16 +53,31 @@ def run_evaluation(dataset_path: str):
         evaluate(
             evaluation_name=evaluation_name,
             data=dataset_path,
-            evaluators={"similarity": similarity_eval},
-            evaluator_config={
-                "similarity": {
-                    "column_mapping": {
-                        "query": "${data.query}",
-                        "ground_truth": "${data.ground_truth}",
-                        "response": "${data.response}",
-                    }
-                }
+            evaluators={
+                "groundedness": groundedness_eval,
+                "relevance": relevance_eval,
             },
+            # evaluator_config={    # For single-turn chats
+            #     "similarity": {
+            #         "column_mapping": {
+            #             "query": "${data.query}",
+            #             "response": "${data.response}",
+            #             "ground_truth": "${data.ground_truth}",
+            #         }
+            #     },
+            #     "groundedness": {
+            #         "column_mapping": {
+            #             "context": "${data.context}",
+            #             "response": "${data.response}",
+            #         }
+            #     },
+            #     "relevance": {
+            #         "column_mapping": {
+            #             "query": "${data.query}",
+            #             "response": "${data.response}",
+            #         },
+            #     },
+            # },
             azure_ai_project=azure_ai_project,  # Makes the evaluation results available in the Foundry UI
             output_path=output_path,
         )
@@ -68,5 +85,8 @@ def run_evaluation(dataset_path: str):
 
 
 if __name__ == "__main__":
-    dataset_path = "datasets/example.csv"
+    dataset_path = "datasets/conversations_repeated_call.jsonl"
+    # dataset_path = "datasets/conversation_example.jsonl"
+    # dataset_path = "datasets/single_turn_example.csv"
+
     run_evaluation(dataset_path)
