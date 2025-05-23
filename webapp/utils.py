3522ad9445aa11550a813a6979bbcfc072bfa3cc
Daniel van den Haak <daniel.vandenhaak@kpn.com>
2025-05-23 16:30:25 +0200

    Add util function to send msg from db

--- webapp/utils.py
+++ webapp/utils.py
@@ -1,21 +1,30 @@
 import json
 
 import streamlit as st
-from azure.servicebus import ServiceBusClient
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
+from sqlalchemy import Engine, select
+from sqlalchemy.orm import Session
 
 from repeated_calls.utils.loggers import Logger
+from repeated_calls.database.tables import CallEvent as CallEventRow
+from repeated_calls.database.schemas import CallEvent
 
 logger = Logger()
 
 @st.cache_resource
-def get_sb_client(fqn: str, key: str) -> ServiceBusClient:
-    client = ServiceBusClient(
-        fully_qualified_namespace=fqn,
-        credential=key,
-    )
+def get_sb_client(connection_string: str) -> ServiceBusClient:
+    client = ServiceBusClient.from_connection_string(connection_string)
     logger.info(f"ServiceBusClient created with endpoint: sb://{client.fully_qualified_namespace}")
 
     return client
+
+
+@st.cache_resource
+def get_sql_client() -> Engine:
+    from repeated_calls.database import engine
+
+    return engine
+
 
 @st.cache_data
 def load_scenarios(path: str) -> list[dict]:
@@ -24,3 +33,25 @@ def load_scenarios(path: str) -> list[dict]:
     logger.info(f"Loaded {len(data)} scenarios from {path}")
 
     return data
+
+def send_msg(id: int, client: ServiceBusClient, queue: str, engine: Engine) -> None:
+    # Retrieve scenario from database
+    q = select(CallEventRow).where(CallEventRow.id == id)
+    with Session(engine) as session:
+        res = session.execute(q).scalar()
+
+    if res is None:
+        raise ValueError(f"CallEvent with ID {id} not found in the database.")
+    else:
+        # Validate the data using Pydantic
+        event = CallEvent(**res.__dict__)
+
+    # Send the message to the queue
+    with client:
+        with client.get_queue_sender(queue) as sender:
+            msg = ServiceBusMessage(body=json.dumps(event.model_dump(mode="json")))
+            sender.send_messages(msg)
+
+            logger.info(f"Message sent to queue {queue} with CallEvent ID {event.id}")
+    
+    
