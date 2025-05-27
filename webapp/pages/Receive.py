d9ee4b113cf3f3f1811e2bce227eb7b98692ef11
Daniel van den Haak <daniel.vandenhaak@kpn.com>
2025-05-27 16:05:08 +0200

    Create MVP for receive page

--- webapp/pages/Receive.py
+++ webapp/pages/Receive.py
@@ -1,6 +1,9 @@
+import asyncio
+
 import streamlit as st
 import utils as su
 
+from repeated_calls.orchestrator.main import run_sequence
 from repeated_calls.streaming.settings import StreamingSettings
 from repeated_calls.utils.loggers import Logger
 
@@ -9,12 +12,27 @@ config = StreamingSettings()
 
 st.set_page_config(page_title="Process Customer Calls", page_icon=":material/call:", layout="wide")
 
+st.title("Process call scenarios")
+
 # Load (cached) ServiceBusClient
 client = su.get_sb_client(config.connection_string)
 
-# TODO: read message queueu
+if st.button("Process message", use_container_width=True):
+    status = st.status("Receiving message...", expanded=True)
 
-# TODO: process message queue messages
-# Convert to Pydantic model using event = CallEvent(**json.loads(mesage))
-# Call orchestrator using await run_sequence(event)
-# Display results
+    event = su.receive_msg(client, config.queue)
+
+    if event is None:
+        status.update(label="No messages found!", state="error")
+        st.stop()
+
+    st.subheader("CallEvent")
+    st.write(event.model_dump(mode="json"))  # TODO: format nicely
+
+    # Process the message in the orchestrator
+    status.update(label="Processing message...", state="running")
+    res = asyncio.run(run_sequence(event))
+
+    status.update(label="Message processed successfully!", state="complete")
+    st.subheader("Results")
+    st.write(res.model_dump(mode="json"))  # TODO: format nicely
